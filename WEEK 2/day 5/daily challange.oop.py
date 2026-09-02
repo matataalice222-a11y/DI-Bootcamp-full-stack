4bd63914783799a0a1eb545603755bf71d05100d
Your Name <you@example.com>
2026-09-02 00:38:40 -0700

    daily

--- WEEK 2/day 5/daily challange.oop.py
+++ WEEK 2/day 5/daily challange.oop.py
@@ -1,17 +1,4 @@
 
-### Exercise 1: OOP Quiz Answers
-
-# **Class:** A blueprint or template for creating objects. It defines the attributes (data) and methods (behavior) that the created objects will have.
-# **Instance:** A specific realization of a class (an individual object created using the class template).
-# **Encapsulation:** The practice of bundling data (attributes) and methods that operate on that data into a single unit (a class), while restricting direct external access to internal components.
-# **Abstraction:** Hiding complex implementation details and showing only the essential features of an object to simplify interaction.
-# **Inheritance:** A mechanism where a child class derives attributes and methods from a parent class, promoting code reuse.
-# **Multiple Inheritance:** A feature where a child class can inherit directly from more than one parent class.
-# **Polymorphism:** The ability of different classes to respond to the same method call in their own specific ways.
-#**Method Resolution Order (MRO):** The deterministic order in which Python searches for attributes or methods in a class hierarchy, especially when dealing with multiple inheritance.
-
-
-
 ### Exercise 2: Deck of Cards Implementation
 
 
